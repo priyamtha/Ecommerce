28244b53fd85a3fcc9494c4949d5718c36d53969
priya <priyamtha1509@gmail.com>
2025-03-13 21:40:39 +0530

    Initial commit

---
+++ frontend/src/routes/Routes.jsx
@@ -0,0 +1,11 @@
+// routes/Routes.jsx
+import LoginPage from '../pages/Login'; // Correct path to Login.jsx
+import SignupPage from '../pages/signup';
+import ActivationPage from '../pages/activation'; // Correct path to Activation.jsx
+import HomePage from '../pages/HomePage';
+import CreateProduct from '../pages/createProduct';
+import MyProduct from '../pages/myProduct'; // Fixed casing
+import ProductDetail from '../pages/ProductDetail';
+import Cart from '../pages/cart'; // Fixed casing
+
+export { LoginPage, SignupPage, ActivationPage, HomePage, CreateProduct, MyProduct, ProductDetail, Cart }; // Named export
